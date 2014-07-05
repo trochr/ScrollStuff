6c106f4af2a0d119683b61155915c1dbb433866e
tom <trocher@gmail.com>
2014-07-05 11:46:29 +0200

    Add debug outline

--- autoscroll.js
+++ autoscroll.js
@@ -1,10 +1,10 @@
-// AutoScroll : we compute the scrolling speed by knowing the reading speed of the user
-// and the number of words per line in the paragraph under the mouse
-
+// AutoScroll : we compute the scrolling speed in pixels/seconds by knowing the reading speed of the user,
+// the number of words per line in the paragraph under the mouse, and the height in pixels of the line
 
 var wps=3;
 var interval;
 var scrolling = 1;
+var debug = false;
 
 window.onload = function(){load()};
 
@@ -15,20 +15,25 @@ function load() {
     ps[i].onmouseover = function() {
       onP(this);
     };
+    ps[i].onmouseout = function() {
+      offP(this);
+    };
   }
-  displayStatus();
 }
 
-function displayStatus() {
- var as = document.createElement('div');
- as.id = "asStatus";
- document.body.insertBefore(as,document.body.firstChild);
- document.onmousemove = function(e) {
-   document.body.getElementById('asStatus').setAttribute('style');
+function toggleStatus(elm) {
+ if  (elm.className.match(/hover/) == null ) {
+   elm.className += " " + "hover";
+ }
+ else {
+   elm.className = elm.className.replace(/ hover\b/,'');
  }
 }
 
 function onP(elm) {
+ if (debug) {
+   toggleStatus(elm);
+ }
  pcopy=elm.cloneNode(true);
  elm.parentNode.insertBefore(pcopy, elm.nextSibling);
  pcopy.innerHTML = 'A<br>B<br>C<br>D<br>E';
@@ -36,22 +41,24 @@ function onP(elm) {
  var lineCount=Math.round(elm.offsetHeight/(pcopy.offsetHeight/5));
  var pxPerLine = elm.offsetHeight/lineCount;
  var wordsPerLine = elm.innerHTML.split(' ').length/lineCount;
- console.log("Lines count: "+lineCount+" | mean words per line : "+wordsPerLine);
+// console.log("Lines count: "+lineCount+" | mean words per line : "+wordsPerLine);
  pcopy.parentNode.removeChild(pcopy);
  if (lineCount >= 3) { // only scroll when on a real paragraph
      launchScroll(wordsPerLine,pxPerLine);
  }
 }
 
-function offP() {
-  clearInterval(interval);
+function offP(elm) {
+  if (debug) {
+    toggleStatus(elm);
+  }
 }
 
 function launchScroll(wpl, ppl) {
   var delay = ppl / (wps * wpl);
   clearInterval(interval);
   interval = setInterval(function() {
-    console.log("ppl:" + ppl + " | wps:" + wps + " | wpl:" + wpl + " | pxPerSec:" + delay);
+//    console.log("ppl:" + ppl + " | wps:" + wps + " | wpl:" + wpl + " | pxPerSec:" + delay);
     window.scrollBy(0, 1 * scrolling);
   }, 1000 * delay);
 }
