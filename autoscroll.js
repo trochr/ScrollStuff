f46a98d3d71bb162c3595fe1b654fa86f4f47b10
tom <trocher@gmail.com>
2014-07-06 19:21:28 +0200

    Restrict the divs to min 10 words

--- autoscroll.js
+++ autoscroll.js
@@ -2,7 +2,6 @@
 // the number of words per line in the paragraph under the mouse, and the height in pixels of the line
 
 // Enhancement : show a status with reading speed, info (hit esc to stop), adjust link, debug checkbox
-// Enhancement : restrict the divs to min 20 words 
 
 // bookmarklet : 
 // javascript:(function()%7Bs%3Ddocument.createElement(%27script%27)%3Bs.type%3D%27text/javascript%27%3Bs.src%3D%27file://localhost/Users/tom/Documents/Thomas/Dev/ScrollStuff/autoscroll.js%3Fv%3D%27%2BparseInt(Math.random()*99999999)%3Bdocument.body.appendChild(s)%3B%7D)()%3B
@@ -26,7 +25,7 @@ function loadAS() {
   var psd = document.body.getElementsByTagName('div');
   psd = Array.prototype.slice.call(psd)
   var ps = psp.concat(psd);
-  
+  ps = ps.filter(function(e,i,a){return (e.innerText.split(' ').length>10)}); // Only keep divs with more that 10 words
   for (var i = 0; i < ps.length; i++) {
     ps[i].onmouseover = function() {
       onP(this);
