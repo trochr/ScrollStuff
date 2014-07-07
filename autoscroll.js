61cd3e3ce7a9a5ba6d5b3eab3c535e6d93b75a8f
trochr <trocher@gmail.com>
2014-07-07 17:51:22 +0200

    Don't capture outside divs

--- autoscroll.js
+++ autoscroll.js
@@ -1,10 +1,18 @@
 // AutoScroll : we compute the scrolling speed in pixels/seconds by knowing the reading speed of the user,
 // the number of words per line in the paragraph under the mouse, and the height in pixels of the line
 
-// Enhancement : show a status with reading speed, info (hit esc to stop), adjust link, debug checkbox
+// Enhancement : show a flash status with reading speed, info (hit esc to stop), adjust link, debug checkbox
+// Enhancement : protect from reloading the bookmarklet
+// Enhancement : make it pretty
+// Bug : things are seriously broken on a page with lots of comments 
+// (see http://blogs.gartner.com/anton-chuvakin/2014/06/06/siem-analytics-histories-and-lessons/)
+// Bug : don't capture outside divs. Shall we exclude divs that contains divs ?
 
 // bookmarklet : 
+// Local Mac :
 // javascript:(function()%7Bs%3Ddocument.createElement(%27script%27)%3Bs.type%3D%27text/javascript%27%3Bs.src%3D%27file://localhost/Users/tom/Documents/Thomas/Dev/ScrollStuff/autoscroll.js%3Fv%3D%27%2BparseInt(Math.random()*99999999)%3Bdocument.body.appendChild(s)%3B%7D)()%3B
+// Local PC :
+// javascript:(function()%7Bs%3Ddocument.createElement(%27script%27)%3Bs.type%3D%27text/javascript%27%3Bs.src%3D%27file:///C:/Users/606676623/Documents/Perso/Dev/Scroll/ScrollStuff/autoscroll.js%3Fv%3D%27%2BparseInt(Math.random()*99999999)%3Bdocument.body.appendChild(s)%3B%7D)()%3B
 
 var wordsReadPerSecond=3;
 var interval;
@@ -25,7 +33,17 @@ function loadAS() {
   var psd = document.body.getElementsByTagName('div');
   psd = Array.prototype.slice.call(psd)
   var ps = psp.concat(psd);
-  ps = ps.filter(function(e,i,a){return (e.innerText.split(' ').length>10)}); // Only keep divs with more that 10 words
+  ps = ps.filter(function(e,i,a){ 
+                    var childs = e.childNodes;
+                    childs = Array.prototype.slice.call(childs);
+                    var hasNoDivOrPChilds = true;
+                    childs.forEach(function(elm,i,a){
+                        hasNoDivOrPChilds &= elm.tagName!="DIV" && elm.tagName!="P";
+                    });
+                   var hasMoreThan10words = (e.innerText.split(' ').length>10); 
+                    return hasMoreThan10words && hasNoDivOrPChilds;
+                 }); 
+                   
   for (var i = 0; i < ps.length; i++) {
     ps[i].onmouseover = function() {
       onP(this);
