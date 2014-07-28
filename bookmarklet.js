613ec4052d320af5e9eb740d3370ec7d72c0795a
trochr <trocher@gmail.com>
2014-07-28 10:49:02 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,7 +3,8 @@
 javascript: (function() {
     s = document.createElement('script');
     s.type = 'text/javascript';
-    s.src = 'file:///C:/Users/606676623/Documents/Perso/Dev/Scroll/autoscroll.js?id=7180b572-7dd7-820e-4c64-d2278713adcb&v=' +         parseInt(Math.random() * 99999999);
+    s.src = 'https://rawgithub.com/trochr/ScrollStuff/master/autoscroll.js?id=guid&v=' + 
+    parseInt(Math.random() * 99999999);
     document.body.appendChild(s);
 })();
 
