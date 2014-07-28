282c1bf89f34464e446717c977112ef081f20c77
trochr <trocher@gmail.com>
2014-07-28 10:47:42 +0200

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,9 @@
+// Do not use as-is
+
+javascript: (function() {
+    s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'file:///C:/Users/606676623/Documents/Perso/Dev/Scroll/autoscroll.js?id=7180b572-7dd7-820e-4c64-d2278713adcb&v=' +         parseInt(Math.random() * 99999999);
+    document.body.appendChild(s);
+})();
+
